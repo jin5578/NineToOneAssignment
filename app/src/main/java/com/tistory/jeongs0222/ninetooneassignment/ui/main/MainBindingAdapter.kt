9c5a8d595cfc2783a827685144a0d1a94a416948
정현성 <jin55789@naver.com>
2020-11-01 17:21:18 +0900

    Distance Format 변환 BindingAdapter 수정

--- app/src/main/java/com/tistory/jeongs0222/ninetooneassignment/ui/main/MainBindingAdapter.kt
+++ app/src/main/java/com/tistory/jeongs0222/ninetooneassignment/ui/main/MainBindingAdapter.kt
@@ -1,5 +1,6 @@
 package com.tistory.jeongs0222.ninetooneassignment.ui.main
 
+import android.annotation.SuppressLint
 import android.widget.TextView
 import androidx.databinding.BindingAdapter
 import androidx.recyclerview.widget.RecyclerView
@@ -15,7 +16,17 @@ fun locationList(recyclerView: RecyclerView, list: MutableList<Document>?) {
     }
 }
 
+@SuppressLint("SetTextI18n")
 @BindingAdapter("distanceFormat")
 fun distanceFormat(textView: TextView, distance: String) {
-    textView.text = (distance.toDouble() / 1000.0).toString()
+    val meter = distance.toDouble()
+
+    when(meter > 1000.0) {
+        true ->
+            textView.text = String.format("%.1f", meter / 1000.0) + "km"
+
+        false ->
+            textView.text = distance + "m"
+
+    }
 }
