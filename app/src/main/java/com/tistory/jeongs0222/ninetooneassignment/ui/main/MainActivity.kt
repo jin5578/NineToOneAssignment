bcf2c906cd04d5dcc236e580a3f1cd494596b25e
정현성 <jin55789@naver.com>
2020-11-01 15:37:13 +0900

    - 권한 확인 로직 추가
     - 위도, 경도

--- app/src/main/java/com/tistory/jeongs0222/ninetooneassignment/ui/main/MainActivity.kt
+++ app/src/main/java/com/tistory/jeongs0222/ninetooneassignment/ui/main/MainActivity.kt
@@ -1,19 +1,37 @@
 package com.tistory.jeongs0222.ninetooneassignment.ui.main
 
+import android.Manifest
+import android.content.Context
+import android.content.pm.PackageManager
+import android.location.LocationManager
 import android.os.Bundle
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.lifecycle.Observer
 import com.tistory.jeongs0222.ninetooneassignment.R
 import com.tistory.jeongs0222.ninetooneassignment.databinding.ActivityMainBinding
 import com.tistory.jeongs0222.ninetooneassignment.ui.BaseActivity
+import com.tistory.jeongs0222.ninetooneassignment.util.showPermissionAlertDialog
 import org.koin.androidx.viewmodel.ext.android.viewModel
-import timber.log.Timber
 
 
 class MainActivity : BaseActivity<ActivityMainBinding>() {
 
     override val layoutResourceId: Int = R.layout.activity_main
-
     private val viewModel by viewModel<MainViewModel>()
+
+    private val LOCATION_PERMISSION_CODE = 111
+
+    private val permissionCheckList = arrayOf(
+        Manifest.permission.ACCESS_FINE_LOCATION,
+        Manifest.permission.ACCESS_COARSE_LOCATION
+    )
+
+    private val locationManager by lazy { getSystemService(Context.LOCATION_SERVICE) as LocationManager }
+
+    private lateinit var longitude: String
+    private lateinit var latitude: String
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,9 +41,69 @@ class MainActivity : BaseActivity<ActivityMainBinding>() {
             lifecycleOwner = this@MainActivity
         }
 
+        checkPermission()
+
         viewModel.searchText.observe(this, Observer {
 
         })
+    }
+
+    private fun checkPermission() {
+        if (
+            (ContextCompat.checkSelfPermission(
+                applicationContext,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED)
+        ) {
+            requestPermission()
+        } else {
+            requestLocation()
+        }
+    }
+
+    private fun requestPermission() {
+        ActivityCompat.requestPermissions(this, permissionCheckList, LOCATION_PERMISSION_CODE)
+    }
+
+    private fun requestLocation() {
+        val location = if (ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            return
+        } else {
+            locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER)!!
+        }
+
+        longitude = location.longitude.toString()
+        latitude = location.latitude.toString()
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<String>, grantResults: IntArray
+    ) {
+        when (requestCode) {
+            LOCATION_PERMISSION_CODE -> {
+                if ((grantResults.isNotEmpty() &&
+                            grantResults[0] == PackageManager.PERMISSION_GRANTED)
+                ) {
+                    requestLocation()
+                } else {
+                    showPermissionAlertDialog()
+                }
+            }
+        }
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+
+        checkPermission()
     }
 
 }
---
+++ app/src/main/java/com/tistory/jeongs0222/ninetooneassignment/util/Activities.kt
@@ -0,0 +1,21 @@
+package com.tistory.jeongs0222.ninetooneassignment.util
+
+import android.app.Activity
+import android.app.AlertDialog
+import android.content.Intent
+import android.net.Uri
+import android.provider.Settings
+
+
+fun Activity.showPermissionAlertDialog() {
+    AlertDialog.Builder(applicationContext).apply {
+        setTitle("권한 설정")
+        setMessage("해당 앱의 원활한 기능을 이용하시려면 애플리케이션 정보 > 권한에서 모든 권한을 허용해주세요.")
+        setCancelable(false)
+        setPositiveButton("설정") { _, _ ->
+            startActivity(
+                Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS).setData(Uri.parse("package:$packageName"))
+            )
+        }
+    }.create().show()
+}
